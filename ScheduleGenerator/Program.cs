29b1bd9d1dc1fe4f35f4983f69b9344e63e7e441
simasbakus <simas.bakus@gmail.com>
2020-07-01 17:51:04 +0300

    Initial commit

---
+++ ScheduleGenerator/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ScheduleGenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
